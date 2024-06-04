d17bb2d07c36d51af6473bb837a0e8b8cc06c6fb
PhillipCostas <Phillipcostas@gmail.com>
2024-06-04 11:54:22 -0400

    sign-up/sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/user.js");
+
+const bcrypt = require("bcrypt");
+
+
+
 router.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs");
   });
+
+router.post("/sign-up", async (req, res) => {
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and Confirm Password must match.')
+    }
+
+    const userInDatabase = await User.findOne({ username: req.body.username});
+    if (userInDatabase) {
+        return res.send('Username already taken.')
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+        req.body.password = hashedPassword;
+
+    const user = await User.create(req.body);
+        res.send(`Thanks for signing up ${user.username}`)
+
+    res.send("Form submission accepted!");
+  });
+  
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs");
+  });
+  
   
 module.exports = router;
